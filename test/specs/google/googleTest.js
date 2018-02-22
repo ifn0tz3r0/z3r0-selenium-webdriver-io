d24512a7c1b9b27eeaa6d882caa46a64dd8f3053
ifn0tz3r0 <aaronkubela@gmail.com>
2018-02-22 06:49:27 -0800

    constants file for wait timeouts

--- test/specs/google/googleTest.js
+++ test/specs/google/googleTest.js
@@ -2,10 +2,12 @@
 var assert = require('chai').assert
 var expect = require('chai').expect
 
+import constants from '../../../src/constants'
 import pageGoogle from '../../../src/pageobjects/pageGoogle'
 
 describe('google test', function() {
   it('should navigate to google search page, perform verification, and submit a search', function () {
+
 
     console.log(`navigating to ${pageGoogle.url}`)
     browser.url(pageGoogle.url)
@@ -13,7 +15,7 @@ describe('google test', function() {
     console.log(`verifying page title is '${pageGoogle.title}'`)
     assert.equal(title, pageGoogle.title)
 
-    browser.waitForVisible(pageGoogle.elementSearchInput.selector, 50000)
+    browser.waitForVisible(pageGoogle.elementSearchInput.selector, constants.WAIT_LONG)
     browser.setValue(pageGoogle.elementSearchInput.selector, pageGoogle.searchTerm)
 
 
@@ -21,7 +23,7 @@ describe('google test', function() {
     //  workaround to remove auto suggestions, which intermittently interrupts
     //  click on search submit. browser.keys('Escape') works in chrome but not
     //  currently in firefox.
-    let autoSuggestClassName = 'gstl_0 sbdd_a';
+    let autoSuggestClassName = 'gstl_0 sbdd_a'
 
     browser.execute(function(className) {
       (document.getElementsByClassName("gstl_0 sbdd_a")[0]).style.visibility = "hidden";
