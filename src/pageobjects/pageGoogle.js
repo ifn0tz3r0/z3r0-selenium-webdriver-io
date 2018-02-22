f1e4968cee103a1145019e50378dab28712babdc
ifn0tz3r0 <aaronkubela@gmail.com>
2018-02-22 04:25:00 -0800

    first commit

---
+++ src/pageobjects/pageGoogle.js
@@ -0,0 +1,11 @@
+
+
+class pageGoogle {
+
+  constructor(){
+    this.title = 'Google'
+    this.url = 'http://www.google.com'
+  }
+}
+
+export default new pageGoogle()
---
+++ test/specs/google/googleTest.js
@@ -0,0 +1,15 @@
+var assert = require('assert')
+
+import pageGoogle from '../../../src/pageobjects/pageGoogle'
+
+describe('google test', function() {
+  it('should navigate to google search page, perform verification, and submit a search', function () {
+
+    console.log(`navigating to ${pageGoogle.url}`)
+    browser.url(pageGoogle.url)
+    var title = browser.getTitle()
+    console.log(`verifying page title is '${pageGoogle.title}'`)
+    assert.equal(title, pageGoogle.title)
+
+  });
+});
